734f76da465f744a5c71ab8802fc12c60e1d1e68
InnovateBham_12 <InnovateBham_12@Admins-iMac.local>
2017-08-07 11:30:19 -0500

    get full list

--- index.js
+++ index.js
@@ -17,21 +17,43 @@ var pathJSON = path.join(__dirname, 'data.json');
 
 app.get('/chirps', function(req, res){
     res.sendFile(pathJSON);
+   
 });
 
 app.post('/chirps', function(req, res){
   console.log('We are posting');
-  
+
 });
 
 app.put('/chirps/:id', function(req, res){
-
+    console.log('Here\'s where we update');
 });
 
 app.delete('/chirps/:id', function(req, res){
-
+    console.log('Delete the things');
 });
 
 app.get('/chirps/:id', function(req, res) {
- 
+    console.log('get single item');
+    res.sendFile(pathJSON, 'utf-8', function(err, file){
+        if (err){
+            res.status(500);
+            res.send('Unable to find single message.')
+        }
+        var arr = JSON.parse(file);
+        var id = req.params.id;
+        var result;
+
+        arr.forEach(function(chirp){
+            if (chirp.id === id){
+                result = chirp;
+            }
+        });
+
+        if (result) {
+            res.send(result);
+        } else {
+            res.send(404);
+        }
+    })
 });
